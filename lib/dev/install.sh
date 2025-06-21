9bdc28d4ff25bca7beb875b4a5a1317f0c53afe9
Sage(Potat) <nirim000@gmail.com>
2025-06-20 20:05:20 -0400

    Update install.sh

--- lib/dev/install.sh
+++ lib/dev/install.sh
@@ -2,13 +2,13 @@
 dotfiles="$HOME/.ml4w-hyprland/dotfiles"
 
 # share
-sudo cp -r $dotfiles/share/. /usr/share/ml4w-hyprland
+sudo cp -r $dotfiles/share/. /var/usrlocal/share/ml4w-hyprland
 echo ":: share installed"
 
 # lib
-sudo cp -r $dotfiles/lib/. /usr/lib/ml4w-hyprland
+sudo cp -r $dotfiles/lib/. /var/usrlocal/lib/ml4w-hyprland
 echo ":: lib installed"
 
 # bin
-sudo cp $dotfiles/bin/ml4w-hyprland-setup /usr/bin/ml4w-hyprland-setup
+sudo cp $dotfiles/bin/ml4w-hyprland-setup /var/usrlocal/bin/ml4w-hyprland-setup
 echo ":: bin installed"
